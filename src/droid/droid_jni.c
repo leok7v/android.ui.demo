6b62e9d3f9959341ed65a32694c6ea86ba71e7fd
leok7v <leo.kuznetsov@gmail.com>
2020-04-24 11:54:17 -0700

    reduced verbosity increased readability

--- src/droid/droid_jni.c
+++ src/droid/droid_jni.c
@@ -40,90 +40,153 @@ static bool check_and_clear_exception(JNIEnv* env) {
 #define getIntField(...)          (!supported ? 0    : (*env)->GetIntField(env, __VA_ARGS__))
 #define getFloatField(...)        (!supported ? 0    : (*env)->GetFloatField(env, __VA_ARGS__))
 #define getStaticIntField(...)    (!supported ? 0    : (*env)->GetStaticIntField(env, __VA_ARGS__))
-#define callObjectMethod(...)     (!supported ? null : (*env)->CallObjectMethod(env, __VA_ARGS__))
-#define callBooleanMethod(...)    (!supported ? false : (*env)->CallBooleanMethod(env, __VA_ARGS__))
-#define callVoidMethod(...) do { if (supported) { (*env)->CallVoidMethod(env, __VA_ARGS__); } } while (0)
+#define callBooleanMethodV(obj, mid, vl)    (!supported ? false : (*env)->CallBooleanMethodV(env, obj, mid, vl))
+#define callObjectMethodV(obj, mid, vl) (!supported ? null : (*env)->CallObjectMethodV(env, obj, mid, vl))
+#define callVoidMethodV(obj, mid, vl) do { if (supported) { (*env)->CallVoidMethodV(env, obj, mid, vl); } } while (0)
 
 #define not_null(p) (!check_and_clear_exception(env) && (p) != null)
 #define check(p) do { supported = not_null(p); } while (0)
 
+jobject new_object(JNIEnv* env, const char* class_name) {
+    bool supported = true;
+    jobject cls = findClass(class_name); check(cls);
+    jmethodID ctor_mid = getMethod(cls, "<init>", "()V"); check(ctor_mid);
+    jobject object = newObject(cls, ctor_mid); check(object);
+    return supported ? object : null;
+}
+
+static jint get_static_int_(JNIEnv* env, jclass cls, const char* label, bool* ok) {
+    bool supported = true;
+    jfieldID fid = getStaticFieldId(cls, label, "I"); check(fid);
+    jint value = getStaticIntField(cls, fid);
+    *ok = supported;
+    return value;
+}
+
+#define get_static_int(cls, label) (!supported ? 0 : get_static_int_(env, cls, label, &supported))
+
+static jint get_int_field_(JNIEnv* env, jobject obj, const char* label, bool* ok) {
+    bool supported = true;
+    jclass cls = getObjectClass(obj); check(cls);
+    jfieldID fid = getFieldId(cls, label, "I"); check(fid);
+    jint value = getIntField(obj, fid);
+    *ok = supported;
+    return value;
+}
+
+#define get_int_field(cls, label) (!supported ? 0 : get_int_field_(env, cls, label, &supported))
+
+static jfloat get_float_field_(JNIEnv* env, jobject obj, const char* label, bool* ok) {
+    bool supported = true;
+    jclass cls = getObjectClass(obj); check(cls);
+    jfieldID fid = getFieldId(cls, label, "F"); check(fid);
+    jfloat value = getFloatField(obj, fid);
+    *ok = supported;
+    return value;
+}
+
+#define get_float_field(cls, label) (!supported ? 0 : get_float_field_(env, cls, label, &supported))
+
+static bool call_void_method_(JNIEnv* env, jobject obj, const char* label, const char* signature, ...) {
+    bool supported = true;
+    jclass cls = getObjectClass(obj); check(cls);
+    jmethodID mid = getMethod(cls, label, signature); check(mid);
+    va_list vl;
+    va_start(vl, signature);
+    callVoidMethodV(obj, mid, vl);
+    va_end(vl);
+    return !check_and_clear_exception(env) && supported;
+}
+
+#define call_void_method(obj, label, signature, ...) \
+    supported = supported && call_void_method_(env, obj, label, signature, ##__VA_ARGS__)
+
+static jobject call_obj_method_(JNIEnv* env, jobject obj, const char* label, const char* signature,
+                                bool *ok, ...) {
+    bool supported = true;
+    jclass cls = getObjectClass(obj); check(cls);
+    jmethodID mid = getMethod(cls, label, signature); check(mid);
+    va_list vl;
+    va_start(vl, ok);
+    jobject r = callObjectMethodV(obj, mid, vl);
+    va_end(vl);
+    *ok = !check_and_clear_exception(env) && supported;
+    return r;
+}
+
+#define call_obj_method(obj, label, signature, ...) \
+    (!supported ? null : call_obj_method_(env, obj, label, signature, &supported, ##__VA_ARGS__))
+
+static jboolean call_bool_method_(JNIEnv* env, jobject obj, const char* label, const char* signature,
+                                  bool *ok, ...) {
+    bool supported = true;
+    jclass cls = getObjectClass(obj); check(cls);
+    jmethodID mid = getMethod(cls, label, signature); check(mid);
+    va_list vl;
+    va_start(vl, ok);
+    jboolean r = callBooleanMethodV(obj, mid, vl);
+    va_end(vl);
+    *ok = !check_and_clear_exception(env) && supported;
+    return r;
+}
+
+#define call_bool_method(obj, label, signature, ...) \
+    (!supported ? false : call_bool_method_(env, obj, label, signature, &supported, ##__VA_ARGS__))
+
 static jobject get_system_service(ANativeActivity* na, const char* label) {
     JNIEnv* env = na->env;
     bool supported = true;
-    jclass context_class = findClass("android/content/Context");
-    check(context_class);
+    jclass context_class = findClass("android/content/Context"); check(context_class);
     jfieldID service_name_fid = getStaticFieldId(context_class, label, "Ljava/lang/String;");
     check(service_name_fid);
     jstring service_name_string = getStaticObjectField(context_class, service_name_fid);
     check(service_name_string);
-    jmethodID get_system_service_mid = getMethod(context_class, "getSystemService", "(Ljava/lang/String;)Ljava/lang/Object;");
-    check(get_system_service_mid);
-    jobject service = callObjectMethod(activity_this(na), get_system_service_mid, service_name_string);
+    jobject service = call_obj_method(activity_this(na), "getSystemService",
+                        "(Ljava/lang/String;)Ljava/lang/Object;", service_name_string);
     check(service);
     return !supported ? null : service;
 }
 
-
 bool droid_jni_hide_navigation_bar(ANativeActivity* na) {
     JNIEnv* env = na->env;
     bool supported = true;
-    jclass    activity_class   = findClass("android/app/NativeActivity");
-    jmethodID getWindow_mid    = getMethod(activity_class, "getWindow", "()Landroid/view/Window;");
-    jclass    window_class     = findClass("android/view/Window");
-    jmethodID getDecorView_mid = getMethod(window_class, "getDecorView", "()Landroid/view/View;");
-    jclass    viewClass        = findClass("android/view/View");
-    jmethodID set_system_ui_visibility = getMethod(viewClass, "setSystemUiVisibility", "(I)V");
-    jobject   window           = callObjectMethod(activity_this(na), getWindow_mid);
-    jobject   decorView        = callObjectMethod(window, getDecorView_mid);
-    jfieldID  flag_fullscreen_fid       = getStaticFieldId(viewClass, "SYSTEM_UI_FLAG_FULLSCREEN", "I");
-    jfieldID  flag_hide_navigation_fid  = getStaticFieldId(viewClass, "SYSTEM_UI_FLAG_HIDE_NAVIGATION", "I");
-    jfieldID  flag_immersive_fid        = getStaticFieldId(viewClass, "SYSTEM_UI_FLAG_IMMERSIVE", "I");
-    jfieldID  flag_immersive_sticky_fid = getStaticFieldId(viewClass, "SYSTEM_UI_FLAG_IMMERSIVE_STICKY", "I");
-    jfieldID  flag_layout_stable_fid    = getStaticFieldId(viewClass, "SYSTEM_UI_FLAG_LAYOUT_STABLE", "I");
-    jfieldID  flag_layout_fulscreen_fid = getStaticFieldId(viewClass, "SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN", "I");
-    jfieldID  flag_layout_hide_navigation_fid = getStaticFieldId(viewClass, "SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION", "I");
-    const int flag_fullscreen       = getStaticIntField(viewClass, flag_fullscreen_fid);
-    const int flag_hide_navigation  = getStaticIntField(viewClass, flag_hide_navigation_fid);
-    const int flag_immersive        = getStaticIntField(viewClass, flag_immersive_fid);
-    const int flag_immersive_sticky = getStaticIntField(viewClass, flag_immersive_sticky_fid);
-    const int flag_layout_stable    = getStaticIntField(viewClass, flag_layout_stable_fid);
-    const int flag_layout_fulscreen = getStaticIntField(viewClass, flag_layout_fulscreen_fid);
-    const int flag_layout_hide_navigation = getStaticIntField(viewClass, flag_layout_hide_navigation_fid);
-    const int flag = flag_fullscreen | flag_hide_navigation | flag_immersive | flag_immersive_sticky |
-                     flag_layout_fulscreen | flag_layout_hide_navigation| flag_layout_stable;
-    callVoidMethod(decorView, set_system_ui_visibility, flag);
-    return !check_and_clear_exception(env);
+    jobject window = call_obj_method(activity_this(na), "getWindow", "()Landroid/view/Window;");
+    check(window);
+    jobject decor_view = call_obj_method(window, "getDecorView", "()Landroid/view/View;");
+    check(decor_view);
+    jclass cls = findClass("android/view/View"); check(cls);
+    const int fullscreen       = get_static_int(cls, "SYSTEM_UI_FLAG_FULLSCREEN");
+    const int hide_navigation  = get_static_int(cls, "SYSTEM_UI_FLAG_HIDE_NAVIGATION");
+    const int immersive        = get_static_int(cls, "SYSTEM_UI_FLAG_IMMERSIVE");
+    const int immersive_sticky = get_static_int(cls, "SYSTEM_UI_FLAG_IMMERSIVE_STICKY");
+    const int layout_stable    = get_static_int(cls, "SYSTEM_UI_FLAG_LAYOUT_STABLE");
+    const int layout_fulscreen = get_static_int(cls, "SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN");
+    const int layout_hide_navigation = get_static_int(cls, "SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION");
+    const int flag = fullscreen | hide_navigation | immersive | immersive_sticky |
+                     layout_fulscreen | layout_hide_navigation| layout_stable;
+    call_void_method(decor_view, "setSystemUiVisibility", "(I)V", flag);
+    return supported;
 }
 
 bool droid_jni_vibrate_with_effect(ANativeActivity* na, const char* effect) { // API 26+
     JNIEnv* env = na->env;
     jobject vibrator_service = get_system_service(na, "VIBRATOR_SERVICE");
     bool supported = vibrator_service != null;
-    jclass vibrator_service_class = getObjectClass(vibrator_service);
-    check(vibrator_service_class);
     jclass vibration_effect_class = findClass("android/os/VibrationEffect");
     check(vibration_effect_class);
-    jfieldID effect_fid = getStaticFieldId(vibration_effect_class, effect, "I");
-    check(effect_fid);
-    const int effect_int = getStaticIntField(vibration_effect_class, effect_fid);
+    const int effect_int = get_static_int(vibration_effect_class, effect);
     supported = !check_and_clear_exception(env) && supported;
-    jmethodID vibrate_mid = getMethod(vibrator_service_class, "vibrate", "(Landroid/os/VibrationEffect)V");
-    check(vibrate_mid);
-    callVoidMethod(vibrator_service, vibrate_mid, effect_int);
-    return !check_and_clear_exception(env) && supported;
+    call_void_method(vibrator_service, "vibrate", "(Landroid/os/VibrationEffect)V", effect_int);
+    return supported;
 }
 
 bool droid_jni_vibrate_milliseconds(ANativeActivity* na, int milliseconds) {
     JNIEnv* env = na->env;
     jobject vibrator_service = get_system_service(na, "VIBRATOR_SERVICE");
     bool supported = vibrator_service != null;
-    jclass vibrator_service_class = getObjectClass(vibrator_service);
-    check(vibrator_service_class);
-    jmethodID vibrate_mid = getMethod(vibrator_service_class, "vibrate", "(J)V");
-    check(vibrate_mid);
     jlong ms = milliseconds;
-    callVoidMethod(vibrator_service, vibrate_mid, ms);
-    return !check_and_clear_exception(env) && supported;
+    call_void_method(vibrator_service, "vibrate", "(J)V", ms);
+    return supported;
 }
 
 bool droid_jni_show_keyboard(ANativeActivity* na, bool on, int flags) {
@@ -131,90 +194,45 @@ bool droid_jni_show_keyboard(ANativeActivity* na, bool on, int flags) {
     JNIEnv* env = na->env;
     jobject activity = activity_this(na);
     bool supported = true;
-    jclass nac = getObjectClass(activity);
-    jobject ims = get_system_service(na, "INPUT_METHOD_SERVICE");
-    check(ims);
-    jclass ims_class = getObjectClass(ims);
-    check(ims_class);
-    jmethodID show_soft_input_mid = getMethod(ims_class, "showSoftInput", "(Landroid/view/View;I)Z");
-    check(show_soft_input_mid);
-    jmethodID hide_soft_input_mid = getMethod(ims_class, "hideSoftInputFromWindow", "(Landroid/os/IBinder;I)Z");
-    check(hide_soft_input_mid);
-    jmethodID get_window_mid = getMethod(nac, "getWindow", "()Landroid/view/Window;");
-    check(get_window_mid);
-    jclass window_class = findClass("android/view/Window");
-    check(window_class);
-    jmethodID get_decor_view_mid = getMethod(window_class, "getDecorView", "()Landroid/view/View;");
-    check(get_decor_view_mid);
-    jobject window = callObjectMethod(activity, get_window_mid);
+    jobject ims = get_system_service(na, "INPUT_METHOD_SERVICE"); check(ims);
+    jobject window = call_obj_method(activity, "getWindow", "()Landroid/view/Window;");
     check(window);
-    jobject decor_view = callObjectMethod(window, get_decor_view_mid);
+    jobject decor_view = call_obj_method(window, "getDecorView", "()Landroid/view/View;");
     check(decor_view);
     bool done = false;
     if (supported) {
         if (on) {
-            done = callBooleanMethod(ims, show_soft_input_mid, decor_view, 0);
+            done = call_bool_method(ims, "showSoftInput", "(Landroid/view/View;I)Z", decor_view, 0);
         } else {
-            jclass view_class = findClass("android/view/View");
-            check(view_class);
-            jmethodID get_window_token_mid = getMethod(view_class, "getWindowToken", "()Landroid/os/IBinder;");
-            check(get_window_token_mid);
-            jobject binder = callObjectMethod(decor_view, get_window_token_mid);
+            jobject binder = call_obj_method(decor_view, "getWindowToken", "()Landroid/os/IBinder;");
             check(binder);
-            done = callBooleanMethod(ims, hide_soft_input_mid, binder, 0);
+            done = call_bool_method(ims, "hideSoftInputFromWindow", "(Landroid/os/IBinder;I)Z", binder, 0);
         }
         if (!done) { traceln("done=%d happens on back button", done); }
     }
     return !check_and_clear_exception(env) && supported;
 }
 
-jobject new_object(JNIEnv* env, const char* class_name) {
-    bool supported = true;
-    jobject cls = findClass(class_name);
-    check(cls);
-    jmethodID ctor_mid = getMethod(cls, "<init>", "()V");
-    check(ctor_mid);
-    jobject object = newObject(cls, ctor_mid);
-    check(object);
-    return supported ? object : null;
-}
-
 void droid_jni_get_display_real_size(ANativeActivity* na, droid_display_metrics_t* m) {
     JNIEnv* env = na->env;
     bool supported = true;
-    jobject ws = get_system_service(na, "WINDOW_SERVICE");
-    check(ws);
-    jclass ws_class = getObjectClass(ws);
-    check(ws_class);
+    jobject ws = get_system_service(na, "WINDOW_SERVICE"); check(ws);
+    jclass ws_class = getObjectClass(ws); check(ws_class);
     jobject display_metrics = new_object(env, "android/util/DisplayMetrics");
     check(display_metrics);
-    jmethodID get_default_display_mid = getMethod(ws_class, "getDefaultDisplay", "()Landroid/view/Display;");
-    check(get_default_display_mid);
-    jobject display = callObjectMethod(ws, get_default_display_mid);
+    jobject display = call_obj_method(ws, "getDefaultDisplay", "()Landroid/view/Display;");
     check(display);
-    jobject display_class = getObjectClass(display);
-    check(display_class);
-    jmethodID get_real_metrics_mid = getMethod(display_class, "getRealMetrics", "(Landroid/util/DisplayMetrics;)V");
-    check(get_real_metrics_mid);
-    jobject dm = new_object(env, "android/util/DisplayMetrics");
-    check(dm);
-    callVoidMethod(display, get_real_metrics_mid, dm);
+    jobject dm = new_object(env, "android/util/DisplayMetrics"); check(dm);
+    call_void_method(display, "getRealMetrics", "(Landroid/util/DisplayMetrics;)V", dm);
     if (supported) {
         jclass dm_class = getObjectClass(dm); check(dm_class);
-        jfieldID xdpi_fid = getFieldId(dm_class, "xdpi", "F"); check(xdpi_fid);
-        jfieldID ydpi_fid = getFieldId(dm_class, "ydpi", "F"); check(ydpi_fid);
-        jfieldID width_pixels_fid   = getFieldId(dm_class, "widthPixels", "I");   check(width_pixels_fid);
-        jfieldID height_pixels_fid  = getFieldId(dm_class, "heightPixels", "I");  check(height_pixels_fid);
-        jfieldID density_fid        = getFieldId(dm_class, "density", "F");       check(density_fid);
-        jfieldID density_dpi_fid    = getFieldId(dm_class, "densityDpi", "I");    check(density_dpi_fid);
-        jfieldID scaled_density_fid = getFieldId(dm_class, "scaledDensity", "F"); check(scaled_density_fid);
-        m->w       = getIntField(dm, width_pixels_fid);
-        m->h       = getIntField(dm, height_pixels_fid);
-        m->xdpi    = getFloatField(dm, xdpi_fid);
-        m->ydpi    = getFloatField(dm, ydpi_fid);
-        m->dpi     = getIntField(dm, density_dpi_fid);
-        m->density = getFloatField(dm, density_fid);
-        m->scaled_density = getFloatField(dm, scaled_density_fid);
+        m->w = get_int_field(dm, "widthPixels");
+        m->h = get_int_field(dm, "heightPixels");
+        m->xdpi = get_float_field(dm, "xdpi");
+        m->ydpi = get_float_field(dm, "ydpi");
+        m->dpi = get_int_field(dm, "densityDpi");
+        m->density = get_float_field(dm, "density");
+        m->scaled_density = get_float_field(dm, "scaledDensity");
     } else {
         memset(m, 0, sizeof(*m));
     }
